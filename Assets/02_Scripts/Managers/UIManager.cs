c16e601bda6f9c95fdca377623cb337c2d03e7dc
tmdrb7214 <tmdrb7214@naver.com>
2025-02-25 12:28:05 +0900

    refactor 버튼

--- Assets/02_Scripts/Managers/UIManager.cs
+++ Assets/02_Scripts/Managers/UIManager.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    SelectManager selectManager;
+    GameManager gameManager;
+
     [SerializeField] private GameObject nextStageButton;
     [SerializeField] private GameObject previousStageButton;
 
     [SerializeField] private GameObject startUI;
     [SerializeField] private GameObject characterUI;
     [SerializeField] private GameObject settingUI;
-
     public void toglePanel(int selectPanelNumber)
     {
         switch (selectPanelNumber)
@@ -32,15 +36,40 @@ public class UIManager : MonoBehaviour
 
     public void NextStageButton()
     {
-       int number = GameManager.Instance.SelectManager.GetSelectedStageIndex();
-        //if()
-        GameManager.Instance.SelectManager.SetSelectedStageIndex( number);
+        int number = GameManager.Instance.SelectManager.GetSelectedStageIndex();
+        if (number < 2)
+        {
+            int nextNumber = number + 1;
+            GameManager.Instance.SelectManager.SetSelectedStageIndex(nextNumber);
+            selectManager.UpdateStageUI();
+
+            previousStageButton.SetActive(true);
+            if (nextNumber == 2)
+            {
+                nextStageButton.SetActive(false);
+            }
+            
+        }
     }
 
     public void PreviousStageButton()
     {
+        int number = GameManager.Instance.SelectManager.GetSelectedStageIndex();
+        if (number > 0)
+        {
+            int nextnumber = number - 1;
+            GameManager.Instance.SelectManager.SetSelectedStageIndex(nextnumber);
+            selectManager.UpdateStageUI();
 
+            nextStageButton.SetActive(true);
+            if (nextnumber == 0)
+            {
+                previousStageButton.SetActive(false);
+            }
+            
+        }
     }
+
 
 
     /*
